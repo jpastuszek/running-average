8e0a9a4a953412967c4aab446211d0d91737b7b9
Jakub Pastuszek <jpastuszek@gmail.com>
2018-10-12 17:31:10 +0100

    docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,26 @@
+//! This crate provides `RunningAverage` and `RealTimeRunningAverage` types that provide facility to calculate running averages with specified time windows width and using constant memory.
+//! 
+//! The `RunningAverage` type can be used when processing streams of temporal data while `RealTimeRunningAverage` can be used when measured events are happening in real time.
+
 use std::collections::VecDeque;
 use std::time::{Instant, Duration};
 use std::ops::AddAssign;
 use std::iter::Sum;
 use std::default::Default;
 
+/// Types implementing this trait can be used as Instant type in TimeSource trait and for RunningAverage
 pub trait TimeInstant {
+    /// Returns Duration elapsed since given TimeInstant and Self.
     fn duration_since(&self, since: Self) -> Duration;
+    /// Forward Self by given Duration into future.
     fn forward(&mut self, duration: Duration);
 }
 
+/// Types implementing this trait can be used as TimeSource for RealTimeRunningAverage.
 pub trait TimeSource {
+    /// Type implementing TimeInstant for this TimeSource.
     type Instant: TimeInstant + Copy;
+    /// Return current Instant.
     fn now(&self) -> Self::Instant;
 }
 
@@ -24,6 +34,7 @@ impl TimeInstant for Instant {
     }
 }
 
+/// TimeSource that uses real time clock via `Instant::now()`.
 #[derive(Debug)]
 pub struct RealTimeSource;
 impl TimeSource for RealTimeSource {
@@ -53,6 +64,7 @@ impl TimeInstant for f64 {
     }
 }
 
+/// TimeSource that has to be manually progressed forward via `ManualTimeSource::time_shift()` method.
 #[derive(Debug)]
 pub struct ManualTimeSource {
     now: f64,
@@ -78,37 +90,44 @@ impl ManualTimeSource {
     }
 }
 
+/// Represent result of the calculation of running average
 #[derive(Debug)]
-pub struct Measure<T> {
+pub struct Measurement<T> {
     value: T, 
     duration: Duration,
 }
 
 use std::fmt;
-impl<T> fmt::Display for Measure<T> where T: Clone + fmt::Display + ToRate, <T as ToRate>::Output: Into<f64> {
+impl<T> fmt::Display for Measurement<T> where T: Clone + fmt::Display + ToRate, <T as ToRate>::Output: Into<f64> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:.3}", self.rate().into())
     }
 }
 
-impl<T> Measure<T> {
+impl<T> Measurement<T> {
+    /// Returns pointer to internal value of the measurement which is sum of all samples within time window
     pub fn value(&self) -> &T {
         &self.value
     }
 
+    /// Returns internal value of the measurement which is sum of all samples within time window consuming self
     pub fn unwrap(self) -> T {
         self.value
     }
 
+    /// Calculates actual running average value based on sum of all samples and width of the time window
     pub fn rate(&self) -> <T as ToRate>::Output where T: Clone + ToRate {
         self.value.clone().to_rate(self.duration)
     }
 
+    /// Calculates actual running average value based on sum of all samples and width of the time window consuming self
     pub fn to_rate(self) -> <T as ToRate>::Output where T: ToRate {
         self.value.to_rate(self.duration)
     }
 }
 
+/// Represents running average calculation window.
+/// It is using specified window width that will consist of given number of accumulator buckets to ensure constant memory usage.
 #[derive(Debug)]
 pub struct RunningAverage<V: Default, I: TimeInstant + Copy> {
     window: VecDeque<V>,
@@ -117,16 +136,19 @@ pub struct RunningAverage<V: Default, I: TimeInstant + Copy> {
 }
 
 impl<V: Default, I: TimeInstant + Copy> Default for RunningAverage<V, I> {
+    /// Crate new RunningAverage instance with window of 8 seconds width and 16 buckets.
     fn default() -> RunningAverage<V, I> {
         RunningAverage::new(Duration::from_secs(8))
     }
 }
 
 impl<V: Default, I: TimeInstant + Copy> RunningAverage<V, I> {
+    /// Crate new RunningAverage instance that will average over window of width of given duration using 16 buckets.
     pub fn new(duration: Duration) -> RunningAverage<V, I> {
         RunningAverage::with_capacity(duration, 16)
     }
 
+    /// Crate new RunningAverage instance that will average over window of width of given duration with specific number of buckets to use.
     pub fn with_capacity(duration: Duration, capacity: usize) -> RunningAverage<V, I> {
         assert!(capacity > 0, "RunningAverage capacity cannot be 0");
         RunningAverage {
@@ -155,23 +177,27 @@ impl<V: Default, I: TimeInstant + Copy> RunningAverage<V, I> {
         }
     }
     
+    /// Insert value to be average over at given time instant.
     /// Panics if now is less than previous now - time cannot go backwards
     pub fn insert(&mut self, now: I, val: V) where V: AddAssign<V> {
         self.shift(now);
         *self.window.front_mut().unwrap() += val;
     }
 
-    /// Panics if now is less than previous now - time cannot go backwards
-    pub fn measure<'i>(&'i mut self, now: I) -> Measure<V> where V: Sum<&'i V> {
+    /// Calculate running average using time window ending at given time instant.
+    /// Panics if now is less than previous now - time cannot go backwards.
+    pub fn measurement<'i>(&'i mut self, now: I) -> Measurement<V> where V: Sum<&'i V> {
         self.shift(now);
 
-        Measure {
+        Measurement {
             value: self.window.iter().sum(),
             duration: self.duration,
         }
     }
 }
 
+/// Represents running average calculation window where `shift` and `measurement` are using given time source to obtain value of `now` instant.
+/// It is using specified window width that will consist of given number of accumulator buckets to ensure constant memory usage.
 #[derive(Debug)]
 pub struct RealTimeRunningAverage<V: Default, TS: TimeSource = RealTimeSource> {
     inner: RunningAverage<V, TS::Instant>,
@@ -185,7 +211,8 @@ impl<V: Default> Default for RealTimeRunningAverage<V, RealTimeSource> {
 }
 
 impl<V: Default> RealTimeRunningAverage<V, RealTimeSource> {
-    // Note: new() is parametrising output to RealTimeSource as this cannot be inferred otherwise
+    /// Crate new instance with window of given width duration and using RealTimeSource as time source for `now` instant.
+    /// Note: new() is parametrizing output to RealTimeSource as this cannot be inferred otherwise.
     pub fn new(duration: Duration) -> RealTimeRunningAverage<V, RealTimeSource> {
         let time_source = RealTimeSource;
 
@@ -197,6 +224,7 @@ impl<V: Default> RealTimeRunningAverage<V, RealTimeSource> {
 }
 
 impl<V: Default, TS: TimeSource> RealTimeRunningAverage<V, TS> {
+    /// Crate new instance with window of given width duration and using given as time source for `now` instant.
     pub fn with_time_source(duration: Duration, capacity: usize, time_source: TS) -> RealTimeRunningAverage<V, TS> {
         RealTimeRunningAverage {
             inner: RunningAverage::with_capacity(duration, capacity),
@@ -204,29 +232,34 @@ impl<V: Default, TS: TimeSource> RealTimeRunningAverage<V, TS> {
         }
     }
 
+    /// Insert value to be average over now.
+    /// Panics if time source time goes backwards.
     pub fn insert(&mut self, val: V) where V: AddAssign<V> {
         let now = self.time_source.now();
         self.inner.insert(now, val)
     }
     
-    /// Sum of window in duration
-    pub fn measure<'i>(&'i mut self) -> Measure<V> where V: Sum<&'i V> {
+    /// Calculate running average using time window ending now.
+    /// Panics if time source time goes backwards.
+    pub fn measurement<'i>(&'i mut self) -> Measurement<V> where V: Sum<&'i V> {
         let now = self.time_source.now();
-        self.inner.measure(now)
+        self.inner.measurement(now)
     }
 
+    /// Return mutable reference to time source used.
     pub fn time_source(&mut self) -> &mut TS {
         &mut self.time_source
     }
 }
 
+/// Types implementing this trait can be used to calculate `Measurement::rate()` from.
+/// Note: This is not implemented for u64 as it cannot be converted precisely to f64 - use f64 instead for big numbers
+/// Note: Duration can be converted to f64 but will be rounded to fit in it so it is not 100% precise for max Duration
 pub trait ToRate {
     type Output;
     fn to_rate(self, duration: Duration) -> Self::Output;
 }
 
-//Note: This is not implemented for u64 as it cannot be converted precisely to f64 - use f64 instead for big numbers
-//Note: Duration can be converted to f64 but will be rounded to fit in it so it is not 100% precise for max Duration
 impl<T: Into<f64>> ToRate for T {
     type Output = f64;
 
@@ -253,8 +286,8 @@ mod tests {
             tw.time_source().time_shift(1.0);
             tw.insert(10);
 
-            assert_eq!(tw.measure().unwrap(), 40, "for capacity {}: {:?}", capacity, tw);
-            assert_eq!(tw.measure().to_rate(), 10.0, "for capacity {}: {:?}", capacity, tw);
+            assert_eq!(tw.measurement().unwrap(), 40, "for capacity {}: {:?}", capacity, tw);
+            assert_eq!(tw.measurement().to_rate(), 10.0, "for capacity {}: {:?}", capacity, tw);
         }
     }
 
@@ -271,8 +304,8 @@ mod tests {
             tw.time_source().time_shift(1.0);
             tw.time_source().time_shift(1.0);
 
-            assert_eq!(tw.measure().unwrap(), 20, "for capacity {}: {:?}", capacity, tw);
-            assert_eq!(tw.measure().to_rate(), 5.0, "for capacity {}: {:?}", capacity, tw);
+            assert_eq!(tw.measurement().unwrap(), 20, "for capacity {}: {:?}", capacity, tw);
+            assert_eq!(tw.measurement().to_rate(), 5.0, "for capacity {}: {:?}", capacity, tw);
         }
     }
 
@@ -286,8 +319,8 @@ mod tests {
         tw.insert(10);
 
         // Note: this may fail as it is based on real time
-        assert_eq!(tw.measure().unwrap(), 20, "default: {:?}", tw);
-        assert_eq!(tw.measure().to_rate(), 2.5, "default: {:?}", tw);
+        assert_eq!(tw.measurement().unwrap(), 20, "default: {:?}", tw);
+        assert_eq!(tw.measurement().to_rate(), 2.5, "default: {:?}", tw);
     }
 
     #[test]
@@ -300,8 +333,8 @@ mod tests {
         tw.insert(10.0);
 
         // Note: this may fail as it is based on real time
-        assert_eq!(tw.measure().unwrap(), 20.0, "default: {:?}", tw);
-        assert_eq!(tw.measure().to_rate(), 2.5, "default: {:?}", tw);
+        assert_eq!(tw.measurement().unwrap(), 20.0, "default: {:?}", tw);
+        assert_eq!(tw.measurement().to_rate(), 2.5, "default: {:?}", tw);
     }
 
     #[test]
@@ -320,12 +353,12 @@ mod tests {
         tw.time_source().time_shift(1.0);
         tw.insert(10);
 
-        assert_eq!(tw.measure().unwrap(), 40, "long: {:?}", tw);
-        assert_eq!(tw.measure().to_rate(), 10.0, "long: {:?}", tw);
+        assert_eq!(tw.measurement().unwrap(), 40, "long: {:?}", tw);
+        assert_eq!(tw.measurement().to_rate(), 10.0, "long: {:?}", tw);
     }
 
     #[test]
-    fn measure_display() {
+    fn measurement_display() {
         use super::*;
 
         let mut tw = RealTimeRunningAverage::default();
@@ -333,6 +366,6 @@ mod tests {
         tw.insert(10);
         tw.insert(10);
 
-        assert_eq!(&format!("{}", tw.measure()), "2.500");
+        assert_eq!(&format!("{}", tw.measurement()), "2.500");
     }
 }
